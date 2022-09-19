04a4ea7a53bd064b7dfd9f91520b1400df64b6a5
ahmedabzk <ahmed.abzk@gmail.com>
2022-09-19 15:00:25 +0300

    rendering html templates in go

--- main.go
+++ main.go
@@ -9,15 +9,17 @@ import (
 const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request){
+	renderingTemplate(w, "home.page.html")
 
 }
 
 func About(w http.ResponseWriter, r *http.Request){
+	renderingTemplate(w, "about.page.html")
 
 }
 
 func renderingTemplate(w http.ResponseWriter, tmpl string){
-	parsedTemplate, _ := template.ParseFiles("./templates" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
 	 err := parsedTemplate.Execute(w, nil)
 	 if err != nil{
 		fmt.Println("error parsing template", err)
