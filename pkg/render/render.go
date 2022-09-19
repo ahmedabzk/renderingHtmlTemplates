0a072b7855d47c89ca8bdbb25b8e4ac6ebf6c9b2
ahmedabzk <ahmed.abzk@gmail.com>
2022-09-19 16:40:14 +0300

    rendering html templates in go

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
+
+var functions = template.FuncMap{
+
+}
 
 func RenderingTemplate(w http.ResponseWriter, tmpl string){
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
@@ -12,4 +17,49 @@ func RenderingTemplate(w http.ResponseWriter, tmpl string){
 	 if err != nil{
 		fmt.Println("error parsing template", err)
 	 }
+}
+
+func renderingTest(w http.ResponseWriter) (map[string]*template.Template, error){
+
+	myCache := map[string]*template.Template{}
+
+	pages,err := filepath.Glob("./templates/*.page.html")
+
+	if err != nil{
+		fmt.Println("error occured", err)
+		return myCache, err
+	}
+
+	for _, page := range pages{
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+
+		if err != nil{
+			fmt.Println("error occured", err)
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.html")
+		if err != nil{
+			fmt.Println("error occured", err)
+			return myCache, err
+			}
+
+		if len(matches) > 0{
+			ts, err := ts.ParseGlob("./templates/*.layout.html")
+
+			if err != nil{
+			fmt.Println("error occured", err)
+			return myCache, err
+			}
+
+			myCache[name] = ts
+		}
+
+		
+	}
+
+	return myCache, nil
+	
 }
