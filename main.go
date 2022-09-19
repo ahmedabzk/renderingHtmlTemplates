bac959ef03d4276455ccffe1c16acd064480c0c0
ahmedabzk <ahmed.abzk@gmail.com>
2022-09-19 14:36:54 +0300

    rendering html templates in go

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request){
+
+}
+
+func About(w http.ResponseWriter, r *http.Request){
+
+}
+
+func main(){
+	fmt.Println("starting server at port", portNumber)
+
+	http.HandleFunc("/",Home)
+	http.HandleFunc("/about", About)
+
+	http.ListenAndServe(portNumber, nil)
+
+}
